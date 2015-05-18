b56af27b489812d82c0f656e31fbc471fcf97a4f
driesdroesbeke <dries.droesbeke@gmail.com>
2015-05-18 19:47:45 +0200

    Create Carts.php

---
+++ src/SolidShopsApi/Services/Carts.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SolidShopsApi\Services;
+
+class Carts extends \SolidShopsApi\Services\Base {
+	protected $_entity = "carts";
+
+	public function getlist($arr_option = null) {
+		return parent::getlist($arr_option);
+	}
+
+	public function get($cartId) {
+		return parent::get($cartId);
+	}
+
+	public function create($input) {
+		$api_endpoint = "/" . $this->_entity;
+		
+		$obj_response = $this->httpPost ( $api_endpoint ,$input);
+		
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+		
+		return $obj_jsonresponse;
+	}
+	
+	public function addItem($cartId,$input) {
+		$api_endpoint = "/" . $this->_entity."/".$cartId."/items";
+	
+		$obj_response = $this->httpPost ( $api_endpoint ,$input);
+	
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+		return $obj_jsonresponse;
+	}
+	
+	public function removeItem($cartId,$itemId) {
+		$api_endpoint = "/" . $this->_entity."/".$cartId."/items/".$itemId;
+	
+		$obj_response = $this->httpDelete ( $api_endpoint);
+	
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+		return $obj_jsonresponse;
+	}
+
+
+	public function update($cartId,$input) {
+		$api_endpoint = "/" . $this->_entity."/".$cartId;
+		
+		$obj_response = $this->httpPut ( $api_endpoint ,$input);
+		
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+		
+		return $obj_jsonresponse;
+	}
+}
