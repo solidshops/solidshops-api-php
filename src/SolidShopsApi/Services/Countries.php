507c86e324f8cd563e7f4c4aec537dc0df88cf73
driesdroesbeke <dries.droesbeke@gmail.com>
2015-06-09 19:16:16 +0200

    Update Countries.php

--- src/SolidShopsApi/Services/Countries.php
+++ src/SolidShopsApi/Services/Countries.php
@@ -2,7 +2,7 @@
 namespace SolidShopsApi\Services;
 class Countries extends \SolidShopsApi\Services\Base {
 
-	protected $_entity = "shipping";
+	protected $_entity = "countries";
 	public function getlist($arr_option = null) {
 		return parent::getlist($arr_option);
 	}
