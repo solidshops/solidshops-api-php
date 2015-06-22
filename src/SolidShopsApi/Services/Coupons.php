606bd9819dc9d72767f5e4f9b439298da69973d5
driesdroesbeke <dries.droesbeke@gmail.com>
2015-06-22 09:33:03 +0200

    Update Coupons.php

--- src/SolidShopsApi/Services/Coupons.php
+++ src/SolidShopsApi/Services/Coupons.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace SolidShopsApi\Services;
+class Coupons extends \SolidShopsApi\Services\Base {
+	protected $_entity = "coupons";
+	public function getlist($arr_option = null) {
+		return parent::getlist($arr_option);
+	}
+}
