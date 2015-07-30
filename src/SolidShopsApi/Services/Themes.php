9e1afb154a8f17176c6bd04c778bc922e7503ea7
driesdroesbeke <dries.droesbeke@gmail.com>
2015-07-30 09:25:44 +0200

    Create Themes.php

---
+++ src/SolidShopsApi/Services/Themes.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SolidShopsApi\Services;
+
+class Themes extends \SolidShopsApi\Services\Base {
+	protected $_entity = "themes";
+
+	public function getlist($arr_option = null) {
+		return parent::getlist($arr_option);
+	}
+	public function get($id) {
+		return parent::get($id);
+	}
+	
+	public function create($input) {
+		$api_endpoint = "/" . $this->_entity;
+	
+		$obj_response = $this->httpPost ( $api_endpoint ,$input);
+	
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+		return $obj_jsonresponse;
+	}
+	
+	public function update($id,$input) {
+		$api_endpoint = "/" . $this->_entity."/".$id;
+	
+		$obj_response = $this->httpPut ( $api_endpoint ,$input);
+	
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+		return $obj_jsonresponse;
+	}
+	
+	public function delete($id) {
+		$api_endpoint = "/" . $this->_entity."/".$id;
+	
+		$obj_response = $this->httpDelete ( $api_endpoint);
+	
+		$obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+		return $obj_jsonresponse;
+	}
+	
+}
