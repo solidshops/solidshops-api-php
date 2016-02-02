adcc28826823409f22fe17cc5f59859a486b41d6
Dries Droesbeke <dries@solidshops.com>
2016-02-02 09:22:29 +0100

    add user update function

--- src/SolidShopsApi/Services/Users.php
+++ src/SolidShopsApi/Services/Users.php
@@ -11,4 +11,14 @@ class Users extends \SolidShopsApi\Services\Base {
 	public function get($id) {
 		return parent::get($id);
 	}
+	
+	public function update($id,$input) {
+	    $api_endpoint = "/" . $this->_entity."/".$id;
+	
+	    $obj_response = $this->httpPut ( $api_endpoint ,$input);
+	
+	    $obj_jsonresponse = $obj_response->toJsonResponse ();
+	
+	    return $obj_jsonresponse;
+	}
 }
